cd36ce0fdd7bc220e3c33d81de1215d9aebe8de5
UP240643 <up240643@alumnos.upa.edu.mx>
2025-02-20 13:28:17 -0600

    DIA02

---
+++ DIA02.py
@@ -0,0 +1,44 @@
+# SUSTRACTION
+print(3-4)
+# DIVISION
+print(3/4)
+# ADDITION
+print(3+4)
+# FLOOR DISIVION OPERATOR
+print(3//4)
+# MODULUS
+print(3%4)
+# EXPONENTIAL
+print(3**4)
+# MULTIPLICATION
+print(3*4)
+
+#YOUR NAME
+print("MY NAME IS EDUARDO")
+
+# YOUR FAMILY NAME
+print("MY MOTHERS NAMES KARINA")
+
+# YOUR COUNTRY
+print("MI COUNTRYS AGUASCALIENTES")
+
+print("IM ENJOYING 30 DAYS OF PYTHON")
+
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["ASABENET","PYTHON", "FINDLAND"]))
+print(type("HUGO EDUARDO"))
+print("KARINA")               
+print(type("SAN FRANCISCO"))
+
+Nombre = "Eduardo"
+Apellido = "ALMAGUER"
+Nombre_completo = "EDUARDO ALMAGUER NAVARRO"
+país = "Mexico"
+ciudad = "Aguascalientes"
+edad = "18"
+año = "2006"
+is_married = "no"
+is_true = "yes"
+A, B, C = "a", "bueno", "xd"
